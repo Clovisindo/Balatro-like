504edf3af8fc80a6d2f4bdb6413ce10e7ad3ce85
Clovis <kiliranshe@gmail.com>
2024-09-17 17:29:31 +0200

    Correcion auto ordenar
    Control para que cuando el HBoxContainer reordene las cartas, ajuste el alto en funcion del estado, obviando la carta selecionada.
    Corregimos que en swap_cards cuando guardabamos la posicion, no teniamos en cuenta el estado anterior para dar la nueva posicion a la carta movida.

--- scenes/hand_handler/hand_container.gd
+++ scenes/hand_handler/hand_container.gd
@@ -49,11 +49,19 @@ func parent_index(card_slot):
 
 func swap_cards(index):
 	is_crossing = true
-
-	var origin_position = cards_slots[index].position
+	var origin_position =  cards_slots[index].position
+	if cards_slots[index].selected:#guardamos la posicion en funcion del estado
+		origin_position.y -=  selected_card.select_state_pos_y
 	self.move_child(selected_card, index)
 	selected_card.update_new_position(origin_position)
-
 	is_crossing = false
-
 	cards_slots = $".".get_children()
+
+
+func _on_sort_children() -> void:
+	print("test on sort")
+	cards_slots = $".".get_children()
+	for card in cards_slots:
+		if card is Card:
+			if card.selected and selected_card != card :
+				card.position.y = -50
